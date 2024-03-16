45fbb10ed0b301af0789122c6c47246ab28f5695
mihit <mihitsreejith2@gmail.com>
2024-03-17 01:09:50 +0530

    Multiple Return Sequence

--- models/task_inference_model_pipeline.py
+++ models/task_inference_model_pipeline.py
@@ -121,7 +121,7 @@ class MetaICLModel(object):
             
             
             with torch.no_grad():
-                outputs = self.model.generate(input_ids=input_ids, pad_token_id=data.tokenizer.eos_token_id, attention_mask=attention_mask,max_new_tokens = 200)
+                outputs = self.model.generate(input_ids=input_ids, pad_token_id=data.tokenizer.eos_token_id, attention_mask=attention_mask,max_new_tokens = 200,do_sample=True,temperature = 0.7,num_return_sequences = 5,top_k = 50)
                 
                 #print(outputs.shape)
                 #print("\n",len(outputs))
